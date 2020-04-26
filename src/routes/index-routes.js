22fe921f3205b88f58a73cf21e1042c1360171d1
Lucas Frederico Mançan <lucasfmancan@hotmail.com>
2020-04-26 11:52:55 -0300

    Lucas

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -6,7 +6,11 @@ const route = router.get('/', index);
 
 function index(request, response){
     response.status(200).send({
-            title: "Node Api is running...",
+            title: "Prescription System Api is running...",
+            company:"Medtech",
+            authors:[
+                "Lucas F. Mançan"
+                ,"Mikhael Belkovsky"],
             version: "0.0.1"
     });
 }
